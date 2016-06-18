782e8af760f395b05b6eff4b24d68e5e7ecbbe79
blake <blakegeist@gmail.com>
2016-06-18 02:44:22 -0700

    another config attempt

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Blog
     # config.i18n.default_locale = :de
 
     #this preconfigs the assest for heroku / it doesn't show the images if you dont use this
-    config.serve_static_assets = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
