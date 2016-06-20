fe85404fc4386b760eae0aa68a25d53efb071a48
blake <blakegeist@gmail.com>
2016-06-19 22:21:43 -0700

    added heading to styles

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -15,7 +15,6 @@ def index
 
   @dtk_cards = Card.where(set: 'DTK').sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 10)
 
-
   @standard = [
     ['Shadows over Innistrad', @soi_cards, soi_index_path],
     ['Welcome Deck 2016', @w16_cards, w16_index_path],
