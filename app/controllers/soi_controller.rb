2436e2075d8b593565b5fd384f488cc57280d77c
blake <blakegeist@gmail.com>
2016-06-22 23:17:18 -0700

    soi

--- app/controllers/soi_controller.rb
+++ app/controllers/soi_controller.rb
@@ -5,32 +5,32 @@ class SoiController < ApplicationController
   end
 
   def red
-    @soi_cards = Card.where("colors like ?", "%Red%").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
+    @soi_cards = Card.where(set: 'SOI').where("colors like ?", "%Red%").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
     render template: "soi/index"
   end
 
   def blue
-    @soi_cards = Card.where("colors like ?", "%Blue%").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
+    @soi_cards = Card.where(set: 'SOI').where("colors like ?", "%Blue%").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
     render template: "soi/index"
   end
 
   def black
-    @soi_cards = Card.where("colors like ?", "%Black%").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
+    @soi_cards = Card.where(set: 'SOI').where("colors like ?", "%Black%").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
     render template: "soi/index"
   end
 
   def green
-    @soi_cards = Card.where("colors like ?", "%Green%").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
+    @soi_cards = Card.where(set: 'SOI').where("colors like ?", "%Green%").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
     render template: "soi/index"
   end
 
   def white
-    @soi_cards = Card.where("colors like ?", "%White%").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
+    @soi_cards = Card.where(set: 'SOI').where("colors like ?", "%White%").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
     render template: "soi/index"
   end
 
   def colorless
-    @soi_cards = Card.where("colors like ?", "").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
+    @soi_cards = Card.where(set: 'SOI').where("colors like ?", "").sort_by{ |t| t.number }.paginate(:page => params[:page], :per_page => 28)
     render template: "soi/index"
   end
 
