fcfc7f8db156dda90630061b66cbbad7cd30180d
blake <blakegeist@gmail.com>
2016-08-18 20:33:00 -0700

    teste

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 class DashboardsController < ApplicationController
+
   def index
+    
     @cards = current_user.pcards.all.sort_by{ |t| [t.card_id.to_i] }
 
     @user = current_user
@@ -7,7 +9,10 @@ class DashboardsController < ApplicationController
     if current_user.admin?
 
       @cards = Card.all.sort_by{ |t| [t.name] }
+
       render action: "admin_index"
+
     end
   end
+
 end
