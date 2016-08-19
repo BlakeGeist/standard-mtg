dfbe299d876ab3a182109c8840aa48254c2e8b84
blake <blakegeist@gmail.com>
2016-08-18 21:49:01 -0700

    gross
    gross

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,10 @@
 class DashboardsController < ApplicationController
 
   def index
-    
-    @cards = current_user.pcards.all.sort_by{ |t| [t.card_id.to_i] }
+
+    @the_users_cards = current_user.pcards.all.sort_by{ |t| [t.card_id.to_i] }
+
+    @the_cards = Card.where(id: @the_users_cards);
 
     @user = current_user
 
