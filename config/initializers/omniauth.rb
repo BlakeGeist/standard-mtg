b9dbe776eb761f87db858ff4f9e11844d4d2dd9c
blake <blakegeist@gmail.com>
2016-09-06 18:35:09 -0700

    commit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+require 'certified'
 Rails.application.config.middleware.use OmniAuth::Builder do
-   provider :ebay, "runame", "devid", "appid", "certid", "siteid", "environment", "auth_type"
+  provider :ebay, "Blake_Geist-BlakeGei-standa-bvslpr", "a036b866-4e0d-49de-b7f6-a45309064be2", "BlakeGei-standard-SBX-5e6e394ea-29d730e6", "SBX-e6e394ea763e-b466-4f1c-9f4e-f991", "0", :sandbox
 end
