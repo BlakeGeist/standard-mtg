17f4239fca2a767127cbeec16d9aa21eb3603322
blake <blakegeist@gmail.com>
2016-09-07 21:38:19 -0700

    prod ebay omniauth
    prod ebay omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 require 'certified'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :ebay, "Blake_Geist-BlakeGei-standa-bvslpr", "a036b866-4e0d-49de-b7f6-a45309064be2", "BlakeGei-standard-SBX-5e6e394ea-29d730e6", "SBX-e6e394ea763e-b466-4f1c-9f4e-f991", "0", :production, "SignIn"
+  provider :ebay, "Blake_Geist-BlakeGei-standa-oysusnr", "a036b866-4e0d-49de-b7f6-a45309064be2", "	BlakeGei-standard-PRD-ee6e394ea-800e1243", "PRD-e6e394ea35e2-df6c-47ae-a28e-25fa", "0", :production, "SignIn"
 end
