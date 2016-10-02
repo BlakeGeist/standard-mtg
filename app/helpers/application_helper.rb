8f9e1ef10baa70670e7ae5a370d562708188e820
Blake Geist <rightfoot@gmail.com>
2016-10-02 10:25:57 -0700

    Revert "Cleanup"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-
-  require 'ebay_client'
-
-  def test_ebay
-
-    user_t = {:appID=>'BlakeGei-standard-SBX-5e6e394ea-29d730e6'}
-
-    EbayClient.api.get_session_id!(user_t[:appID])
-
-  end
-
 end
