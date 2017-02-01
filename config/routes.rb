7797e533eb02c8a6e9965037c593226b33d0f224
blake <blakegeist@gmail.com>
2017-02-01 01:00:22 -0800

    hooked
    hooked

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'registrations'}
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :users do
     resources :pcards
