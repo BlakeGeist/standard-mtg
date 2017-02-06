dd8ea402e9291a369a9baa3ad2193b1074957ae8
blake <blakegeist@gmail.com>
2017-02-06 00:03:02 -0800

    try that
    try that

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.provider = 'ebay'
-      user.ebay_token = auth.info.ebaytkn
+      user.ebay_token = auth.ebaytkn
     end
   end
 
