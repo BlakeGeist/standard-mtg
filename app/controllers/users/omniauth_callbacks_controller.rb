637c8adb11d06b0d57eca132c317d3ecb1f48c08
blake <blakegeist@gmail.com>
2017-02-07 00:34:03 -0800

    asdf

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      session["dick"] = request.env['omniauth.auth']
+      session["dick"] = request.env['omniauth.auth'] 
       sign_in_and_redirect @user #this will throw if @user is not activated
     else
       session["devise.ebay_data"] = request.env['omniauth.auth']
