9ac1608bab726d276f151fe2cc510c383f14d6d3
blake <blakegeist@gmail.com>
2017-02-06 22:47:45 -0800

    update
    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def ebay
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Ebay") if is_navigational_format?
     else
       session["devise.ebay_data"] = request.env["omniauth.auth"]
