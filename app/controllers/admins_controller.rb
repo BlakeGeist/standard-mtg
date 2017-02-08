bbc3109eaed94b834bf2715dceb8b6a7e53aa54a
blake <blakegeist@gmail.com>
2017-02-07 20:18:59 -0800

    Matt or Ben?
    Matt or Ben?

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -92,20 +92,6 @@ class AdminsController < ApplicationController
 
   def getTokenStatus
 
-    require 'ebayr'
-
-    Ebayr.dev_id = "a036b866-4e0d-49de-b7f6-a45309064be2"
-
-    Ebayr.app_id = "BlakeGei-standard-PRD-ee6e394ea-800e1243"
-
-    Ebayr.cert_id = "PRD-bff3fe44f0ea-09ae-470f-9222-6e2c"
-
-    Ebayr.ru_name = "Blake_Geist-BlakeGei-standa-oysusnr"
-
-    Ebayr.sandbox = false
-
-    @GetOrders = Ebayr.call(:GetSellerList, :EndTimeFrom => '2017-01-07', :EndTimeTo => '2017-02-07', :UserID => session["dick"]["info"]["ebay_id"], :GranularityLevel => 'Coarse', :Pagination => [:EntriesPerPage => 200, :PageNumber => 1], :auth_token => session["dick"]["info"]["ebay_token"])
-
     respond_to do |format|
       format.js
     end
