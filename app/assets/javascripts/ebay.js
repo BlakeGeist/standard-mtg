b3cd5d8ff408232a7f0fdb3df0c7f4dfbb725588
blake <blakegeist@gmail.com>
2017-02-08 09:08:46 -0800

    not popular
    not popular

--- app/assets/javascripts/ebay.js
+++ app/assets/javascripts/ebay.js
@@ -1,7 +1,5 @@
-
-
-  $(document).on('click', '[data-make-active]', function(ev) {
-    event.preventDefault();
-    event.stopPropagation();
-    $(this).parent().parent().toggleClass('is-active');
-  });
+$(document).on('click', '[data-make-active]', function(ev) {
+  event.preventDefault();
+  event.stopPropagation();
+  $(this).parent().parent().toggleClass('is-active');
+});
