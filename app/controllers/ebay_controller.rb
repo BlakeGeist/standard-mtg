99de2606df3cd7032d401fc53b4eab143c790c85
blake <blakegeist@gmail.com>
2017-02-07 22:19:14 -0800

    does it work like this
    does it work like this

--- app/controllers/ebay_controller.rb
+++ app/controllers/ebay_controller.rb
@@ -16,7 +16,7 @@ class EbayController < ApplicationController
 
     Ebayr.sandbox = false
 
-    @GetOrders = Ebayr.call(:GetSellerList, :EndTimeFrom => '2017-01-07', :EndTimeTo => '2017-02-07', :UserID => session["dick"]["info"]["ebay_id"], :GranularityLevel => 'Coarse', :Pagination => [:EntriesPerPage => 200, :PageNumber => 1], :auth_token => session["dick"]["info"]["ebay_token"])
+    @GetOrders = Ebayr.call(:GetMyeBaySelling, :EndTimeFrom => '2017-01-07', :EndTimeTo => '2017-02-07', :UserID => session["dick"]["info"]["ebay_id"], :GranularityLevel => 'Coarse', :Pagination => [:EntriesPerPage => 200, :PageNumber => 1], :auth_token => session["dick"]["info"]["ebay_token"])
 
     respond_to do |format|
       format.js
