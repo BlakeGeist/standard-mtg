1f731562cb5c9f030cfb2fed92ffb2fd90aa1d73
blake <blakegeist@gmail.com>
2017-02-08 19:46:03 -0800

    update gem and add section
    update gem and add section

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908014655) do
+ActiveRecord::Schema.define(version: 20170206074532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -160,6 +160,7 @@ ActiveRecord::Schema.define(version: 20160908014655) do
     t.boolean  "admin"
     t.string   "provider"
     t.string   "uid"
+    t.string   "ebay_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
